7855caceb2ee1e831fc95b3d30363019235202a0
srikanth1801 <srikanthsrikanth2677@gmail.com>
2025-05-26 14:15:56 +0530

    Added FirstApiApp

---
+++ Day 16 - 26.05.2025/FirstApiApp/Contollers/SampleController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class SampleController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string),StatusCodes.Status404NotFound)]
+    
+    public ActionResult GetGreet()
+    {
+        return Ok("Hello World");
+    }
+}
